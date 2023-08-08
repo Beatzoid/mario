bc59daf691bcdce59554ae57bd78d5b4cdb5dc31
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-08-08 10:13:55 -0700

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mario
+
+This is a 2D game engine that will eventually be used to make a Mario clone
+
+# Credit
+
+[This](https://www.youtube.com/playlist?list=PLtrSb4XxIVbp8AKuEAlwNXDxr99e3woGE) Youtube series
---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,110 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+/**
+ * Manages the windows in the engine.
+ * This is a Singleton to prevent multiple instances from being created
+ * and interfering with each other
+ */
+public class Window {
+    private static Window window = null;
+
+    private int width, height;
+    private String title;
+
+    private long glfwWindow;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    /**
+     * Run the window
+     */
+    public void run() {
+        System.out.println("Hello from LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    /**
+     * Initialize the window
+     */
+    public void init() {
+        // Error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW Window");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    /**
+     * Main window loop
+     */
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+
+            // RGBA
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            // Clear the screen with the color above
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+
+    /**
+     * Get and return the current Window instance
+     */
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+}
